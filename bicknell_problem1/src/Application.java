930600a0f9e4c2bb673dab7edec0e1e277997c78
kenneth <kenneth.bicknell@knights.ucf.edu>
2020-03-29 13:44:38 -0400

    changed to unique_words

--- bicknell_problem1/src/Application.java
+++ bicknell_problem1/src/Application.java
@@ -4,6 +4,6 @@ public class Application {
     public static void main(String[] args) throws Exception {
         DuplicateRemover duplicateRemover = new DuplicateRemover();
         duplicateRemover.remove("problem1.txt");
-        duplicateRemover.write("output1.txt");
+        duplicateRemover.write("unique_words.txt");
     }
 }
